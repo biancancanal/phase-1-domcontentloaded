47d38f8729eccf483e7450dcb0942ad37ee8e231
Bianca <biancancanal@gmail.com>
2021-03-24 15:50:03 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+//startup code, kick this whole thing off. Adding an event listener to the
+//whole document 
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("text").innerHTML = "This is really cool!";
+})
+
